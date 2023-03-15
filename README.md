2704dc046089238f689ef37811b9a14eae724679
TronZin <charlesdmm20@gmail.com>
2023-03-14 21:18:16 -0500

    build color selector

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Etch-a-sketch
+
+Project meant to showcase the skills the author has when it comes to javascript programming and css/html design.
+
+Etch-a-Sketch is an application that simulates the likes of paint or GIMP at their very basics, allowing
+the user to sketch on a panel with various colors.
--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const button = document.querySelector("button");
 const input = document.querySelector("input");
 const canvas = document.querySelector(".canvas");
+const colors = document.querySelectorAll(".color");
+const customColor = document.querySelector(".customColor");
 
 setCanvasGrid(16);
 button.addEventListener('click',() =>{setCanvasGrid(input.value)});
+colors.forEach(color => color.addEventListener("click",selectColor));
 
 
 
 
 function setCanvasGrid(number) {
     if (number > 100 || number < 16) number = 16;
+    input.value = "";
 
     let frSize = "";
     for (let i = 0; i < number; i++) {
@@ -23,13 +27,34 @@ function setCanvasGrid(number) {
         for (let j = 0; j < number; j++){
             let square = document.createElement("div");
             canvas.appendChild(square);
+
             square.style.gridRowStart = j;
             square.style.gridRowEnd   = j+1;
+
             square.style.gridColumnStart = i;
             square.style.gridColumnEnd   = i+1;
+            
+            square.style.alignSelf = "center";
+            square.style.justifySelf = "center";
+
             square.style.width = "100%";
             square.style.height = "100%";
+            
+            
+            square.addEventListener("mouseover",paintSquare);
         } 
     }
 }
 
+function paintSquare(e) {
+    if (!e.shiftKey) return;
+
+    e.target.style.backgroundColor = colorInUse;
+}
+
+let colorInUse = "black";
+
+function selectColor(e) {
+    colorInUse = window.getComputedStyle(e.target).backgroundColor;
+    customColor.style.backgroundColor = colorInUse;
+}
